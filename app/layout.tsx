44d7182950ea728c729814ee3aa46610a049d5e1
Luispel40 <luispel2011@gmail.com>
2024-09-12 23:36:21 -0300

    feat: add booking item

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google"
 import localFont from "next/font/local";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] })
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
